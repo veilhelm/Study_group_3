56f66c17ada7615385940635470e1b05419d2d8b
veilhelmAlex <veilhelm.alexander@gmail.com>
2020-09-28 22:24:07 -0500

    class 3 advance loops and other methods

---
+++ answers/class_3_answers.js
@@ -0,0 +1,7 @@
+// dado un string de letras random. diganme cual es la letra que más se repite consecutivamente en ese string
+// ejemplo
+//input = "asggbbbcsdwddddwwwwww"
+//output = w
+
+//input= "aabcaaacdaaakaajvdaaavvvvvvv"
+//output= v
