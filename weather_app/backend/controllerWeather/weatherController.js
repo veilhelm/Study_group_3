989cd9124cbf9d80ff717666c095dc5624e5aa74
Johnm-sys <Johnm-sys.jhonmenjura12@gmail.com>
2020-10-26 16:31:59 -0500

    route_model_controller

---
+++ weather_app/backend/controllerWeather/weatherController.js
@@ -0,0 +1,28 @@
+const axios = require("axios")
+const EventEmiter = require("events")
+const modelWeather = require("../modelWeather/weatherModel")
+
+class WeatherController extends EventEmiter {
+    createWeather = async(req, res) => {
+        try {
+
+            const { data } = await axios({
+                method: "GET",
+                baseURL: process.env.BASEURL,
+                url: process.env.URL
+            })
+            const weather = await new modelWeather(data)
+            weather.save()
+            res.status(200).json(weather)
+
+        } catch (error) {
+            console.log(error.mensagge)
+            res.status(400).json(error.menssage)
+        }
+
+
+    }
+}
+
+const weatherController = WeatherController()
+module.exports = weatherController
